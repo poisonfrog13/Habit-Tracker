529349bec4b2ea5b55bd97546517777bf4c019bc
Olena Gut <lena.gut1993@gmail.com>
2024-02-26 15:07:13 +0000

    Setup initial project structure

---
+++ README.md
@@ -0,0 +1,4 @@
+### set DJANGO_SETTINGS_MODULE for development
+```
+export DJANGO_SETTINGS_MODULE=server.settings.develop
+```
---
+++ server/settings/develop.py
@@ -0,0 +1,40 @@
+"""
+Use this module to specify the values of any settings which should
+be used in development.
+
+To do this, simply declare the values as you would in a regular Django
+settings.py file. The values given here can be combined with, or overload values
+given in the base settings module.
+
+It is recommended that you are familliar with server.settings.base in order to
+understand what values you need to change for your project.
+
+Combined example:
+
+ALLOWED_HOSTS += [
+    "172.168.4.0"
+]
+
+Overload example:
+
+DEBUG = False
+"""
+
+# Import base settings
+from server.settings.base import *  # noqa: F403
+
+# Identify the settings module
+SETTINGS_MODULE = "DEVELOP"
+
+# Define your development settings here
+ALLOWED_HOSTS += [  # noqa: F405
+    "*",
+]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+DEVELOP = True
+
+print(
+    f"DJANGO SETTINGS | {SETTINGS_MODULE=} | {DEVELOP=}, {DEBUG=}"  # noqa: F405
+)
